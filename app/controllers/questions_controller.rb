af7b85cab4ed63e72b90c73ae9bbea85d16c5f01
luisa santos <1analuisantos@gmail.com>
2024-01-22 14:33:44 +0000

    Rails stupid coaching challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,24 @@
 class QuestionsController < ApplicationController
+  def ask
+    @example_variable = 'This is an example'
+  end
+
+  def answer
+    question = params[:question]
+    @answer = coach_answer(question)
+  end
+
+  layout 'application'
+
+  private
+
+  def coach_answer(question)
+    if question.downcase == 'i am going to work'
+      'Great!'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
 end
